edec4983a6ba425dd028f5b4b3159b557792e071
Jon Sugar <jonathansugar@mac.com>
2019-10-25 16:39:07 +0100

    wip - refactor seeder.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,49 +1,63 @@
 <?php
 
+use App\Comment;
 use App\Post;
 use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Collection;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        $people = ['jon', 'james', 'rupert', 'kamren', 'danni'];
+        collect(['jon', 'james', 'rupert', 'kamren', 'danni'])->map(function ($familyMemberName) {
 
-        collect($people)->each(function ($person) {
-            $familyMember = factory(User::class)->create([
-                'name' => $person,
-                'email' => "{$person}@example.com",
-                'email_verified_at' => now(),
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                'remember_token' => Str::random(10),
-            ]);
+            return $this->addFamilyMember($familyMemberName);
 
-            factory(Post::class, rand(2, 10))->create([
-                'user_id' => $familyMember->id,
-            ]);
+        })->map(function (User $familyMember) {
+
+            return $this->addPostsForFamilyMember($familyMember);
+
+        })->flatten()->map(function (Post $post) {
+
+            return $this->addCommentsToPost($post);
+
+        })->flatten()->each(function (Comment $comment){
+
+
+
         });
-
-        collect(Post::all())->each(function ($post) {
-            $numberOfComments = rand(0, 6);
-            if ($numberOfComments > 0) {
-                collect(range(1, $numberOfComments, 1))->each(function ($num) use ($post){
-                    $userId = User::all()->pluck('id')->random();
-                    factory(\App\Comment::class)->create([
-                        'user_id' => $userId,
-                        'post_id' => $post->id
-                    ]);
-                });
-            }
-        });
-
-
     }
 
+    protected function addFamilyMember(string $familyMemberName): User
+    {
+        return factory(User::class)->create([
+            'name' => $familyMemberName,
+            'email' => "{$familyMemberName}@example.com",
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+    }
 
+    protected function addPostsForFamilyMember(User $familyMember): Collection
+    {
+        return factory(Post::class, rand(2, 10))->create([
+            'user_id' => $familyMember->id,
+        ]);
+    }
+
+    protected function addCommentsToPost(Post $post): Collection
+    {
+        $comments = [];
+        for ($comment = 1; $comment <= rand(0, 6); $comment++) {
+            $userId = User::all()->pluck('id')->random();
+            $comments[] = factory(Comment::class)->create([
+                'user_id' => $userId,
+                'post_id' => $post->id
+            ]);
+        }
+
+        return collect($comments);
+    }
 }
---
+++ next-steps.md
@@ -0,0 +1,25 @@
+## Features
+
+### Posts
+
+- Add the ability for users to *like* a post.
+- Add the ability for users to *bookmark* a post.
+- Add the ability for users to *share* a post.
+
+### Comments
+
+- Add the ability for users to *like* a comment.
+
+### Dates
+
+- Dates and times should be presented in human friendly format.
+
+### #tag support
+
+- Add the ability for the users to ***tag*** posts and comments with #tags.
+- Add the ability for the users to search for #tags.
+
+### @mention support
+
+- Add the ability for users to ***@mention*** other users in posts and comments.
+- The system should notify users when they have been @mentioned.
