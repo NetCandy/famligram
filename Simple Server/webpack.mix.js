b023beae6d7c141ea909e3edc06f33494fe4a32f
Jon Sugar <jonathansugar@mac.com>
2019-10-25 12:57:47 +0100

    wip - initial design.

--- Simple Server/webpack.mix.js
+++
@@ -1,8 +0,0 @@
-let mix = require('laravel-mix');
-
-mix.postCss('src/app.css', 'public/css', [
-    require('tailwindcss'),
-]).browserSync({
-    proxy: 'localhost:8080',
-    'browser': 'google chrome'
-});
--- public/js/app.js
+++ public/js/app.js
@@ -23389,8 +23389,8 @@ var app = new vue__WEBPACK_IMPORTED_MODULE_0___default.a({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/jsugar/Documents/Sites/famligram/famligram/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/jsugar/Documents/Sites/famligram/famligram/resources/css/app.css */"./resources/css/app.css");
+__webpack_require__(/*! /Users/jsugar/Documents/Sites/famligram/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/jsugar/Documents/Sites/famligram/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
