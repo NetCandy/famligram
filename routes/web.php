b532d4dfc54d253abaf5ad9710152fbc24587b63
Jon Sugar <jonathansugar@mac.com>
2019-11-18 16:34:41 +0000

    wip - add post api endpoint.

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,11 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/', [\App\Http\Controllers\PostController::class, 'index'])->middleware('auth');
 
 Route::get('{profile}', [\App\Http\Controllers\ProfileController::class, 'index']);
+
+
+Route::prefix('api')->group(function () {
+
+    Route::get('posts', [\App\Http\Controllers\Api\PostController::class, 'index'])
+        ->name('api.post.index');
+
+});
